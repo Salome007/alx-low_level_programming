8748076cd1d6b281125e807be145e283dbe8db55
Salome Essien <serenity.tonyl@gmail.com>
2023-04-19 21:41:38 -0700

    a program that performs simple operations

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,10 +1,11 @@
 #include "3-calc.h"
 
 /**
-  * op_add - the function name
-  * @a: parameter of type int .
-  * @b: parameter of type int .
-  * Return: int .
+ * op_add - calculates the sum of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: sum of a and b
  */
 int op_add(int a, int b)
 {
@@ -12,10 +13,11 @@ int op_add(int a, int b)
 }
 
 /**
-  * op_sub - the function name
-  * @a: parameter of type int .
-  * @b: parameter of type int .
-  * Return: int .
+ * op_sub - calculates the difference of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: difference of a and b
  */
 int op_sub(int a, int b)
 {
@@ -23,10 +25,11 @@ int op_sub(int a, int b)
 }
 
 /**
-  * op_mul - the function name
-  * @a: parameter of type int .
-  * @b: parameter of type int .
-  * Return: int .
+ * op_mul - calculates the product of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: product of a and b
  */
 int op_mul(int a, int b)
 {
@@ -34,10 +37,11 @@ int op_mul(int a, int b)
 }
 
 /**
-  * op_div - the function name
-  * @a: parameter of type int .
-  * @b: parameter of type int .
-  * Return: int .
+ * op_div - calculates the division of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: result of the division of a and b
  */
 int op_div(int a, int b)
 {
@@ -45,10 +49,11 @@ int op_div(int a, int b)
 }
 
 /**
-  * op_mod - the function name
-  * @a: parameter of type int .
-  * @b: parameter of type int .
-  * Return: int .
+ * op_mod - calculates the remainder of the division of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: remainder of a divided b
  */
 int op_mod(int a, int b)
 {
